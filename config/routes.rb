4f5d2bcdbc093459a2e9c5c4ae92b0750690bd52
dannysun725 <danny.sun@me.com>
2015-03-02 18:34:51 -0600

    Teacher API Included

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,11 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'teachers#index'
 
-   resources :teachers
-   resources :classrooms
+   namespace :api do
+      resources :teachers
+   end
+
+   # resources :classrooms
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 

5bc82b894296f61445d6b8efe888a3e13f7bdc0a
dannysun725 <danny.sun@me.com>
2015-03-04 00:51:55 -0600

    Added Show Action to Assignments

--- app/controllers/api/assignments_controller.rb
+++ app/controllers/api/assignments_controller.rb
@@ -2,4 +2,8 @@ class Api::AssignmentsController < ApplicationController
    def index
       render json: Assignment.all
    end
+
+   def show
+      render json: Assignment.find(params[:id])
+   end
 end
